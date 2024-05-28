a0ea103356c13a4b5479fcdcd1e40a0e2717f8c4
nicmora <nicmora24@gmail.com>
2024-05-28 16:37:44 -0300

    first commit

---
+++ src/main/java/pe/com/seek/seekcandidatesmanager/SeekCandidatesManagerApplication.java
@@ -0,0 +1,13 @@
+package pe.com.seek.seekcandidatesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekCandidatesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeekCandidatesManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/seek/seekcandidatesmanager/SeekCandidatesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.seek.seekcandidatesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeekCandidatesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
